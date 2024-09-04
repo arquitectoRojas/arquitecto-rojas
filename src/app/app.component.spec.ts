1d76ec9fdff1347294a29ee5ee660c3870970cd1
RenzoDavila <renzo.davila@tecsup.edu.pe>
2024-09-04 17:52:45 -0500

    configurando arquitecto-rojas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'paginas-web'`, () => {
+  it(`should have as title 'arquitecto-rojas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('paginas-web');
+    expect(app.title).toEqual('arquitecto-rojas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('paginas-web app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('arquitecto-rojas app is running!');
   });
 });
